30b3c4167c4fb09ad4b76dcb5b49aff66307910a
laurijuhani15 <late.lehto@gmail.com>
2026-07-30 14:18:55 +0300

    Create request_filter_settings_button.h

---
+++ components/balboa_spa/button/request_filter_settings_button.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "esphome/components/button/button.h"
+#include "../balboaspa.h"
+
+namespace esphome
+{
+    namespace balboa_spa
+    {
+
+        class RequestFilterSettingsButton : public button::Button
+        {
+        public:
+            void set_parent(BalboaSpa *parent);
+
+        protected:
+            void press_action() override;
+
+        private:
+            BalboaSpa *parent_;
+        };
+
+    }  // namespace balboa_spa
+}  // namespace esphome
