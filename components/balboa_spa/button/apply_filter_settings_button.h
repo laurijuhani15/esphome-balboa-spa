adb942ddb26842f1316be98e24de01e52cafa53e
laurijuhani15 <late.lehto@gmail.com>
2026-07-30 12:03:33 +0300

    Update apply_filter_settings_button.h

--- components/balboa_spa/button/apply_filter_settings_button.h
+++ components/balboa_spa/button/apply_filter_settings_button.h
@@ -5,20 +5,20 @@
 
 namespace esphome
 {
-namespace balboa_spa
-{
+    namespace balboa_spa
+    {
 
-class ApplyFilterSettingsButton : public button::Button
-{
-public:
-    void set_parent(BalboaSpa *parent);
+        class ApplyFilterSettingsButton : public button::Button
+        {
+        public:
+            void set_parent(BalboaSpa *parent);
 
-protected:
-    void press_action() override;
+        protected:
+            void press_action() override;
 
-private:
-    BalboaSpa *parent_;
-};
+        private:
+            BalboaSpa *parent_;
+        };
 
-}  // namespace balboa_spa
+    }  // namespace balboa_spa
 }  // namespace esphome
