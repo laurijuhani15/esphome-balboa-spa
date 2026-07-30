66e2e3c5f3ff12d82803863760477f51059fbdd0
laurijuhani15 <late.lehto@gmail.com>
2026-07-30 10:59:34 +0300

    Update __init__.py

--- components/balboa_spa/button/__init__.py
+++ components/balboa_spa/button/__init__.py
@@ -6,19 +6,16 @@ from .. import balboa_spa_ns, BalboaSpa, CONF_SPA_ID
 DEPENDENCIES = ["balboa_spa"]
 
 SyncTimeButton = balboa_spa_ns.class_("SyncTimeButton", button.Button)
-DisableFilter2Button = balboa_spa_ns.class_("DisableFilter2Button", button.Button)
 RequestFaultLogButton = balboa_spa_ns.class_("RequestFaultLogButton", button.Button)
 ClearReminderButton = balboa_spa_ns.class_("ClearReminderButton", button.Button)
 
 CONF_SYNC_TIME = "sync_time"
-CONF_DISABLE_FILTER2 = "disable_filter2"
 CONF_REQUEST_FAULT_LOG = "request_fault_log"
 CONF_CLEAR_REMINDER = "clear_reminder"
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(CONF_SPA_ID): cv.use_id(BalboaSpa),
     cv.Optional(CONF_SYNC_TIME): button.button_schema(SyncTimeButton),
-    cv.Optional(CONF_DISABLE_FILTER2): button.button_schema(DisableFilter2Button),
     cv.Optional(CONF_REQUEST_FAULT_LOG): button.button_schema(RequestFaultLogButton),
     cv.Optional(CONF_CLEAR_REMINDER): button.button_schema(ClearReminderButton),
 })
@@ -26,9 +23,6 @@ CONFIG_SCHEMA = cv.Schema({
 async def to_code(config):
     parent = await cg.get_variable(config[CONF_SPA_ID])
     if conf := config.get(CONF_SYNC_TIME):
-        var = await button.new_button(conf)
-        cg.add(var.set_parent(parent))
-    if conf := config.get(CONF_DISABLE_FILTER2):
         var = await button.new_button(conf)
         cg.add(var.set_parent(parent))
     if conf := config.get(CONF_REQUEST_FAULT_LOG):
