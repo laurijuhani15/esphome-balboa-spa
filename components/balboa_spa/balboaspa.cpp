a426580f41a8adddeba1486e18cd94e0c876d093
laurijuhani15 <late.lehto@gmail.com>
2026-07-30 15:20:56 +0300

    Update balboaspa.cpp

--- components/balboa_spa/balboaspa.cpp
+++ components/balboa_spa/balboaspa.cpp
@@ -253,6 +253,7 @@ namespace esphome
 
         void BalboaSpa::apply_filter_settings()
         {
+            target_filter2_enable = true;   // VAIN TESTI
             send_command = 0x25;
         }
 
