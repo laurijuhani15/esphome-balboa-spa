97f921e958d84f068e39b81c72b789222b0e4e4b
laurijuhani15 <late.lehto@gmail.com>
2026-07-30 15:39:56 +0300

    Update __init__.py

--- components/balboa_spa/text/__init__.py
+++ components/balboa_spa/text/__init__.py
@@ -16,12 +16,14 @@ SpaFilter1StartTimeText = balboa_spa_ns.class_("SpaFilter1StartTimeText", text.T
 SpaFilter1DurationText = balboa_spa_ns.class_("SpaFilter1DurationText", text.Text)
 SpaFilter2StartTimeText = balboa_spa_ns.class_("SpaFilter2StartTimeText", text.Text)
 SpaFilter2DurationText = balboa_spa_ns.class_("SpaFilter2DurationText", text.Text)
+SpaFilter2EnableText = balboa_spa_ns.class_("SpaFilter2EnableText", text.Text)
 
 CONF_SPA_TIME = "spa_time"
 CONF_FILTER1_START_TIME = "filter1_start_time"
 CONF_FILTER1_DURATION = "filter1_duration"
 CONF_FILTER2_START_TIME = "filter2_start_time"
 CONF_FILTER2_DURATION = "filter2_duration"
+CONF_FILTER2_ENABLE = "filter2_enable"
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(CONF_SPA_ID): cv.use_id(BalboaSpa),
@@ -45,6 +47,10 @@ CONFIG_SCHEMA = cv.Schema({
         SpaFilter2DurationText,
         icon=ICON_TIMER,
     ),
+    cv.Optional(CONF_FILTER2_ENABLE): text.text_schema(
+        SpaFilter2EnableText,
+        icon=ICON_WATER,
+    ),
 })
 
 async def to_code(config):
@@ -67,5 +73,9 @@ async def to_code(config):
         cg.add(var.set_parent(parent))
 
     if conf := config.get(CONF_FILTER2_DURATION):
+        var = await text.new_text(conf)
+        cg.add(var.set_parent(parent))
+        
+    if conf := config.get(CONF_FILTER2_ENABLE):
         var = await text.new_text(conf)
         cg.add(var.set_parent(parent))
